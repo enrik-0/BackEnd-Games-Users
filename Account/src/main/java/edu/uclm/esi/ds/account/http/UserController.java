ba6564218a6ae51e4e85ff40b5c1f3593d5a9f97
AngelLuis1 <AngelLuis.Rodriguez1@alu.uclm.es>
2023-05-12 20:04:54 +0200

    Implements confirmation email in register and maps /error
    # Conflicts:
    #	Account/src/main/java/edu/uclm/esi/ds/account/services/UserService.java

--- Account/src/main/java/edu/uclm/esi/ds/account/http/UserController.java
+++ Account/src/main/java/edu/uclm/esi/ds/account/http/UserController.java
@@ -1,5 +1,6 @@
 package edu.uclm.esi.ds.account.http;
 
+import java.io.IOException;
 import java.util.Map;
 import java.util.UUID;
 
@@ -7,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import edu.uclm.esi.ds.account.entities.User;
-import edu.uclm.esi.ds.account.services.EmailService;
+import edu.uclm.esi.ds.account.exceptions.EmailNotSentException;
 import edu.uclm.esi.ds.account.services.UserService;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -43,8 +46,9 @@ public class UserController {
 			this.userService.register(name, email, pwd1);
 		} catch (DataIntegrityViolationException e) {
 			throw new ResponseStatusException(HttpStatus.CONFLICT);
+		} catch (EmailNotSentException | IOException e) {
+			throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error sending confirmation email.");
 		}
-
 	}
 
 	@PutMapping("/login")
@@ -60,5 +64,16 @@ public class UserController {
 	    }
 	    
 	    response.setHeader("sessionID", sessionID);
+	}
+	
+	@GetMapping("/confirm/{tokenId}")
+	public void confirm(HttpServletResponse response, @PathVariable String tokenId) {
+		try {
+			this.userService.confirm(tokenId);
+			response.sendRedirect("http://localhost:4200");
+		} catch (Exception e) {
+			throw new ResponseStatusException(HttpStatus.CONFLICT,
+					"El token no existe o ha caducado");
+		}
 	}
 }
--- Account/src/main/java/edu/uclm/esi/ds/account/services/EmailService.java
+++ Account/src/main/java/edu/uclm/esi/ds/account/services/EmailService.java
@@ -1,12 +1,16 @@
 package edu.uclm.esi.ds.account.services;
 
 import java.io.IOException;
+import java.io.InputStream;
 
-import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONArray;
 import org.springframework.stereotype.Service;
 
+import edu.uclm.esi.ds.account.entities.Token;
 import edu.uclm.esi.ds.account.entities.User;
+import edu.uclm.esi.ds.account.exceptions.EmailNotSentException;
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -15,43 +19,59 @@ import okhttp3.Response;
 
 @Service
 public class EmailService {
-
-	public void sendConfirmationEmail(User user) throws IOException {
+	
+	public void sendConfirmationEmail(User user, Token token) throws EmailNotSentException, IOException {
 		OkHttpClient client = new OkHttpClient().newBuilder().build();
+		
+		String msg = readFile("confirmation_message.html.txt");
+		msg = msg.replace("#nombre#", user.getName());
+		msg = msg.replace("#TOKEN#", token.getId());
+
+		JSONObject jsoTo = new JSONObject()
+				.put("name", user.getName())
+				.put("email", user.getEmail());
+		
+		JSONObject emailParameters = new JSONObject(readFile("sendinblue.parameters.txt"));
+		JSONObject payload = new JSONObject()
+				.put("sender", emailParameters.getJSONObject("sender"))
+				.put("to", new JSONArray().put(jsoTo))
+				.put("subject", emailParameters.getString("subject"))
+				.put("htmlContent", msg);
+
+		Request request = createRequest(emailParameters, payload);
+
+		Response response = client.newCall(request).execute();	
+		if (response.code() >= 400)
+			throw new EmailNotSentException();
+		
+		response.close();
+	}
+
+	private Request createRequest(JSONObject emailParameters, JSONObject payload) throws JSONException {
 		MediaType mediaType = MediaType.parse("application/json");
-		
-		JSONObject jsonBody = createJsonBody(user);
-		
-		RequestBody body = RequestBody.create(mediaType, jsonBody.toString());
+		JSONObject headers = emailParameters.getJSONObject("headers");	
+
+
+		RequestBody body = RequestBody.create(payload.toString(), mediaType);
 		Request request = new Request.Builder()
-		  .url("https://api.sendinblue.com/v3/smtp/email")
+		  .url(emailParameters.getString("endpoint"))
 		  .method("POST", body)
-		  .addHeader("Content-Type", "application/json")
-		  .addHeader("Accept", "application/json")
-		  .addHeader("api-key", "hwzT263EFNpJAyHK")
+		  .addHeader("Content-Type", headers.getString("Content-Type"))
+		  .addHeader("Accept", headers.getString("Accept"))
+		  .addHeader("api-key", headers.getString("api-key"))
 		  .addHeader("Cookie", "__cf_bm=nzdkBS3OtFjVjz9mCuqVqRNePtvYaoyn04kQUlEOulk-1681196846-0-AdJ9l01WIhqfsKYenbeILcYJZ8CrodgjcaDa/6E4otTcGasg9ZjZ7ZFHuFNoI1pIAaHe3FgByr3OI7E0m6RJGkM=")
 		  .build();
 
-		Response response = client.newCall(request).execute();	
+		return request;
 	}
-	
-	private JSONObject createJsonBody(User user) {
-		JSONObject jsonSender = new JSONObject()
-				.put("name", "Games S.A")
-				.put("email", "AngelLuis.Rodriguez1@alu.uclm.es");
 
-		JSONObject jsonTo = new JSONObject()
-				.put("email", user.getEmail())
-				.put("name", user.getName());
-		
-		JSONArray jsonArrayTo = new JSONArray().put(jsonTo);
+	private String readFile(String fileName) throws IOException {
+		ClassLoader classLoader = getClass().getClassLoader();
 
-		JSONObject jsonBody = new JSONObject()
-				.put("sender", jsonSender)
-				.put("to", jsonArrayTo)
-				.put("subject", "Welcome to Games")
-				.put("htmlContent", "Verify your account");
-	
-		return jsonBody;
-	}
+		try (InputStream fis = classLoader.getResourceAsStream(fileName)) {
+			byte[] b = new byte[fis.available()];
+			fis.read(b);
+			return new String(b);
+		}
+	}	
 }
